d656175df1a1bc9e2dc25def4a089bef5604279d
jbcr <51637606+jbcr@users.noreply.github.com>
2019-11-13 10:29:06 +0100

    add comment on interface

--- src/Factory/ContentStructureFactoryInterface.php
+++ src/Factory/ContentStructureFactoryInterface.php
@@ -10,5 +10,15 @@ interface ContentStructureFactoryInterface
     public const MODE_INSERT_ONLY = 2;
     public const MODE_UPDATE_ONLY = 3;
 
+    /**
+     * @param array      $data
+     * @param string     $remoteId
+     * @param string     $language
+     * @param string     $contentType
+     * @param int|string $parentLocations Int for location id or string for remote location id
+     * @param int        $mode            ContentStructureFactoryInterface
+     *
+     * @return false|\CodeRhapsodie\EzDataflowBundle\Model\ContentStructure
+     */
     public function transform(array $data, string $remoteId, string $language, string $contentType, $parentLocations, int $mode = ContentStructureFactoryInterface::MODE_INSERT_OR_UPDATE);
 }
